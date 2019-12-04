5aa93612209eb1f4fd8f85ecb87cb7fd00386a5f
Catarinacoelho1999 <47819624+Catarinacoelho1999@users.noreply.github.com>
2019-12-04 16:28:17 +0000

    Update ex1.js

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,4 +1,8 @@
 //Variáveis tipo let nomeDaVar;
+let posX, posY
+let velX, velY
+let panWidth, padHeight
+let diamBall
 
 function setup() { //código que só corre uma vez
 
@@ -10,11 +14,44 @@ function setup() { //código que só corre uma vez
   sketchCanvas.parent('ex1');
   ///<<< Não fica a faltar createCanvas
 
-
+  posX = random (width);
+  posY = random (height);
+  velX = random (-10,10);
+  velY = random (-10, 10);
+  padWidth = 60;
+  padHeight = 8;
+  diamBall = 10;
+  
 }
 
-function draw() { //código que corre em loop
-  background(220); //podem apagar esta linha!
 
+
+
+  function draw() {
+    background(123, 201, 144);
+    posX = posX + velX;
+    posY = posY + velY;
+    noStroke();
+    ellipse(posX, posY, diamBall);
+    rectMode(CENTER);
+    rect(mouseX,height-10, padWidth, padHeight);
+    rect(mouseX, height-4,70,9);
+
+  if(posX- diamBall/2 <= 0 || posX+diamBall/2 >= width) {
+    velX = velX*-1; //muda o sinal da velocidade
+  }
+    if(posY- diamBall / 2 <= 0) {
+      velY = velY*-1; //muda o sinal da velocidade
+  }
+    if (posY+diamBall/2 >= height) {
+      print ("perdeste");
+      velY= 0;
+      velX= 0;
+  }
+    if (posX-diamBall/2 >= mouseX-padWidth/2
+        && posX+diamBall/2 <= mouseX+padWidth/2 &&
+        posY >= height -10- padHeight/2) {
+        velY = velY*-1; //muda o sinal da velocidade
+        }
 
 }
